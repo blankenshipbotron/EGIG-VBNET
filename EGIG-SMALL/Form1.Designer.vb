b36474027636daa0ff818a487f7a21901fb8f42e
Mike Blankenship <m.blankenship@botron.com>
2025-03-21 14:04:23 -0700

    WORK - First commit

---
+++ EGIG-SMALL/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnSendata = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader7 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader8 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(12, 12)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnConnect.TabIndex = 0
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(104, 14)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 60)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(646, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'btnSendata
+        '
+        Me.btnSendata.Location = New System.Drawing.Point(333, 11)
+        Me.btnSendata.Name = "btnSendata"
+        Me.btnSendata.Size = New System.Drawing.Size(75, 23)
+        Me.btnSendata.TabIndex = 3
+        Me.btnSendata.Text = "Send Data"
+        Me.btnSendata.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5, Me.ColumnHeader6, Me.ColumnHeader7, Me.ColumnHeader8})
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(12, 99)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(646, 189)
+        Me.ListView1.TabIndex = 4
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Scan ID"
+        Me.ColumnHeader1.Width = 100
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Date"
+        Me.ColumnHeader2.Width = 80
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Time"
+        Me.ColumnHeader3.Width = 80
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Temp"
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Humidity"
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "FW ver"
+        '
+        'ColumnHeader7
+        '
+        Me.ColumnHeader7.Text = "Test Voltage"
+        Me.ColumnHeader7.Width = 80
+        '
+        'ColumnHeader8
+        '
+        Me.ColumnHeader8.Text = "Test result"
+        Me.ColumnHeader8.Width = 100
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(665, 317)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.btnSendata)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnSendata As Button
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
+    Friend WithEvents ColumnHeader5 As ColumnHeader
+    Friend WithEvents ColumnHeader6 As ColumnHeader
+    Friend WithEvents ColumnHeader7 As ColumnHeader
+    Friend WithEvents ColumnHeader8 As ColumnHeader
+End Class
---
+++ EGIG-SMALL/Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports System.ComponentModel
+Imports System.IO.Ports
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Public Class Form1
+    'Private WithEvents serialPort1 As New SerialPort()
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        Try
+            If SerialPort1.IsOpen Then
+                SerialPort1.Close()
+            End If
+
+            SerialPort1.PortName = ComboBox1.SelectedItem.ToString()
+            SerialPort1.BaudRate = 115200
+            SerialPort1.Parity = Parity.None
+            SerialPort1.DataBits = 8
+            SerialPort1.StopBits = StopBits.One
+            SerialPort1.DtrEnable = 1
+
+
+            SerialPort1.Open()
+            TextBox1.AppendText("Serial port opened." & vbCrLf)
+
+
+
+        Catch ex As Exception
+            TextBox1.AppendText("Error opening serial port: " & ex.Message & vbCrLf)
+        End Try
+
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' Populate the combo box with available serial ports
+        ComboBox1.Items.AddRange(SerialPort.GetPortNames())
+    End Sub
+
+    Private Sub serialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Try
+            Dim receivedData As String = SerialPort1.ReadLine() ' Read data until a newline character
+            parseData(receivedData)
+
+            'Me.Invoke(Sub() TextBox1.AppendText("Received: " & receivedData & vbCrLf)) ' Update TextBox1 on the UI thread
+            Me.Invoke(Sub() TextBox1.AppendText(receivedData & vbCrLf)) ' Update TextBox1 on the UI thread
+        Catch ex As Exception
+            Me.Invoke(Sub() TextBox1.AppendText("Error receiving data: " & ex.Message & vbCrLf))
+        End Try
+    End Sub
+    Function parseData(ByVal data As String)
+        Dim inputString As String = data
+        Dim parts() As String = inputString.Split(","c)
+
+        MessageBox.Show(parts(0), "Scan ID") ' SCAN ID
+        MessageBox.Show(parts(1), "Date") ' DATE
+        MessageBox.Show(parts(2), "Time") ' TIME
+        MessageBox.Show(parts(3), "Temperature") ' TEMP
+        MessageBox.Show(parts(4), "Humidity") ' HUMIDITY
+        MessageBox.Show(parts(5), "FW Version") ' FW version
+        MessageBox.Show(parts(6), "Test Volatge") ' Test Voltage
+        MessageBox.Show(parts(7), "Test Result") ' Test result
+        ' RED CODE
+
+        'data.timestamp = millis() / 1000;
+        'data.temperature = Random(0, 255);
+        'data.humidity = Random(0, 100);
+        'data.test_voltage = Random(0, 2000);
+        'data.resistance = Random(0, 4294967295);
+
+
+
+
+
+
+    End Function
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If SerialPort1.IsOpen Then
+            SerialPort1.Close()
+        End If
+    End Sub
+
+    Private Sub btnSendata_Click(sender As Object, e As EventArgs) Handles btnSendata.Click
+        TextBox1.Text = ""
+
+        Try
+            If SerialPort1.IsOpen Then
+                SerialPort1.Write("CMD_EEPROM" & vbCr) ' Send the text from TextBox2
+            Else
+                TextBox1.AppendText("Serial port is not open." & vbCrLf)
+            End If
+        Catch ex As Exception
+            TextBox1.AppendText("Error sending data: " & ex.Message & vbCrLf)
+        End Try
+
+
+    End Sub
+End Class
